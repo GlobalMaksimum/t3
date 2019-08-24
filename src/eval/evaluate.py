1d96f142610933d46ad2cc2a0c82c6d09f8ffb08
Omer Kirbiyik <kirbiyikomer@gmail.com>
2019-08-24 17:07:11 +0300

    Report metric stats

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -16,12 +16,26 @@ def compute_IoU(boxA, boxB):
     
     return iou
 
-n_TP = 0
-n_FP = 0
-n_FN = 0
+stats = {
+    0: { # yaya 
+        'total': 0,
+        'n_TP': 0,
+        'n_FP': 0,
+        'n_FN': 0
+    },
+    1: { # arac
+        'total': 0,
+        'n_TP': 0,
+        'n_FP': 0,
+        'n_FN': 0
+    }
+}
+
+precision = lambda TP, FP: TP / (TP + FP) 
+recall = lambda TP, FN: TP / (TP + FN) 
 
 def get_score(y_class, y_pred, pred_ind, IoU_scores, threshold):
-    global n_TP, n_FP, n_FN;
+    global stats;
     scores = []
     temp = []
     
@@ -31,23 +45,23 @@ def get_score(y_class, y_pred, pred_ind, IoU_scores, threshold):
         y_c = y_class[j]
         y_p = y_pred[i]
         if y_c != y_p:
-            n_FP += 1
+            stats[y_c]['n_FP'] += 1
             scores.append(-1.0)
             continue
         IoU_score = IoU_scores[i]
         if IoU_score >= threshold:
             if pred_ind[i] not in temp:
-                n_TP += 1
+                stats[y_c]['n_TP'] += 1
                 scores.append(3 * IoU_score)
         else:
-            n_FP += 1
+            stats[y_c]['n_FP'] += 1
             scores.append(-1 * (1 - IoU_score))
             
         temp.append(pred_ind[i])
     
     # Add not found GT boxes
     scores += [-1.0] * (len(y_class) - len(np.unique(pred_ind)))
-    n_FN += (len(y_class) - len(np.unique(pred_ind)))
+    stats[y_c]['n_FN'] += (len(y_class) - len(np.unique(pred_ind)))
         
     return np.sum(scores)
 
@@ -79,26 +93,47 @@ def main(y_true, y_scores, threshold):
         len_pred, len_gt= len(pred_val), len(gt_val)
         
         n_gt += len_gt
-        
+            
         if not len(gt_val):
             all_scores.append(-1 * len_pred)
             continue
+        
+        u_c, cnt = np.unique(gt_val[:, 1], return_counts=True)
+        for cl, ct in zip(u_c, cnt):
+            stats[cl]['total'] += ct
         
         score_arr = np.zeros((len_pred, len_gt))
         for i in range(len_pred):
             for j in range(len_gt):
                 score_arr[i, j] = compute_IoU(pred_val[i][0], gt_val[j][0])
                 
-        y_class = gt_val[:, 1]
-        y_pred = pred_val[:, 1]
-        scores = score_arr[np.arange(len(score_arr)), score_arr.argmax(axis=1)]
         
-        IoU_score = get_score(y_class, y_pred, score_arr.argmax(axis=1), scores, threshold)
+        if len(pred_val):
+            y_class = gt_val[:, 1]
+            y_pred = pred_val[:, 1]
+            scores = score_arr[np.arange(len(score_arr)), score_arr.argmax(axis=1)]
+
+            IoU_score = get_score(y_class, y_pred, score_arr.argmax(axis=1), scores, threshold)
+        else:
+            IoU_score = -1 * (len(gt_val) - len(pred_val))
         all_scores.append(IoU_score)
-                
-    click.echo(np.sum(all_scores))
-    click.echo(f'n_gt: {n_gt}')
-    click.echo(f'n_TP: {n_TP}, n_FP: {n_FP}, n_FN: {n_FN}')
+    
+    print(stats)
+    click.echo(f'Total Box: {stats[0]["total"] + stats[1]["total"]}')
+    click.echo('\n================ T3 METRIC ================')
+    click.echo(f'{np.sum(all_scores):.3f} over {3 * (stats[0]["total"] + stats[1]["total"])}')
+    click.echo('\n=========== PRECISION & RECALL ============')
+    for k, v in stats.items():
+        cl = 'yaya' if k == 0 else 'arac'
+        if stats[k]['total'] == 0:
+            continue
+            
+        pre = precision(stats[k]['n_TP'], stats[k]['n_FN'])
+        rec = recall(stats[k]['n_TP'], stats[k]['n_FP'])
+        click.echo(f"{cl}: \n\tprecision: {pre:.3f} \n\trecall: {rec:.3f}\n\tavg: {(pre + rec) / 2:.3f}")
+        
+#     click.echo(f'n_gt: {n_gt}')
+#     click.echo(f'n_TP: {n_TP}, n_FP: {n_FP}, n_FN: {n_FN}')
 
 if __name__ == "__main__":
     main()
